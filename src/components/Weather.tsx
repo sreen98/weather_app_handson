c26437004ad90eccf49a22375ac736526798df93
sreen98 <sreenath.premnath@gmail.com>
2023-03-23 23:02:56 +0530

    feat: Implemented weather API and search

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,34 +1,53 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { API_KEY } from "../constants/constants";
 
 const Weather = () => {
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+  const [weatherData, setWeatherData] = useState([]);
+  const handleChange = (e: any) => {
+    switch (e.target.name) {
+      case "city":
+        setCity(e.target.value);
+        break;
+      case "country":
+        setCountry(e.target.value);
+    }
+  };
+  const checkEnable = () => (city === "" ? true : false);
+
+  async function handleSubmit(e: any) {
+    e.preventDefault();
+    if (checkEnable()) {
+      alert("please input city");
+    } else {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}`
+        )
+        .then((response) => setWeatherData(response.data));
+    }
+  }
+
   return (
     <div>
-      <h1>Weather App</h1>
-      <div>
-        <label>
-          Add Location <input type="text" value="Paris" />
-        </label>
-        <button>Search</button>
-      </div>
-
-      <div>
-        <h2>Locations</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Belfast</td>
-            </tr>
-            <tr>
-              <td>New York</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <h1>Weather App </h1>
+      <form>
+        <input
+          type="text"
+          placeholder="city"
+          name="city"
+          onChange={handleChange}
+        ></input>
+        <input
+          type="text"
+          placeholder="County"
+          name="country"
+          onChange={handleChange}
+        ></input>
+        <button onClick={(e) => handleSubmit(e)}>Submit</button>
+      </form>
     </div>
   );
 };
---
+++ src/constants/constants.tsx
@@ -0,0 +1,1 @@
+export const API_KEY = "8d24f1fa8a71fcb191bea40273a45b16";
